566293504d2443af6bac8380918851842b99bac4
Igor Guerrero <igorgue@protonmail.com>
2018-09-18 18:22:34 -0400

    Add .NET solution and NBitcoin submodule to git.

---
+++ LivianoWallet/Liviano.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Liviano;
+
+namespace Liviano.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wallet = new Wallet();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LivianoWallet/LivianoWallet/Liviano.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NBitcoin;
+
+namespace Liviano
+{
+    public class Wallet
+    {
+        public Wallet()
+        {
+            Console.WriteLine($"Key: {new Key().GetWif(Network.Main)}");
+        }
+    }
+}
