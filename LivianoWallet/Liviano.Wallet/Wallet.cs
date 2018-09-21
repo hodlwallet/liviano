c17cdafacfe68366b24938f47a661beb03fa154f
Neil Hernandez <revon210@hotmail.com>
2018-09-21 11:14:06 -0400

    Renamed wallet file from Liviano.cs -> Wallet.cs

---
+++ LivianoWallet/Liviano.Wallet/Wallet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NBitcoin;
+
+namespace Liviano
+{
+    public class Wallet
+    {
+        public Wallet()
+        {
+            Console.WriteLine("Nbitcoin stuff: " + new Key().GetBitcoinSecret(Network.Main).ToWif());
+        }
+    }
+}
