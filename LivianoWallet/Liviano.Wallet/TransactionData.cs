af012e95a291d6ff5f7ba99b9987cf0045ed0f71
Igor Guerrero <igorgue@protonmail.com>
2018-09-25 10:28:35 -0400

    JSON serialization objects and basic transaction data class.

--- LivianoWallet/Liviano.Wallet/TransactionData.cs
+++ LivianoWallet/Liviano.Wallet/TransactionData.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBitcoin;
+using Newtonsoft.Json;
+using Liviano.Utilities.JsonConverters;
 
 namespace Liviano
 {
     public class TransactionData
     {
+        /// <summary>
+        /// Transaction id.
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        [JsonConverter(typeof(UInt256JsonConverter))]
+        public uint256 Id { get; set; }
     }
 }
--- LivianoWallet/Liviano.Wallet/Wallet.cs
+++ LivianoWallet/Liviano.Wallet/Wallet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using Liviano.Utilities.JsonConverters;
 using NBitcoin;
-using NBitcoin.JsonConverters;
 using Newtonsoft.Json;
 
 namespace Liviano
