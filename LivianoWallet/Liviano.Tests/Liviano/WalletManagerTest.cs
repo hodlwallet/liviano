17a7b6a35d75095012514dd082eaa3bba8a1743d
Igor Guerrero <igorgue@protonmail.com>
2019-01-09 13:07:17 -0500

    Fix test... kinda, well the file system test should not check for that instead for 2 or more wallets

--- LivianoWallet/Liviano.Tests/Liviano/WalletManagerTest.cs
+++ LivianoWallet/Liviano.Tests/Liviano/WalletManagerTest.cs
@@ -5,23 +5,50 @@ using NBitcoin;
 using Liviano.Managers;
 using Serilog;
 using System.IO;
+using System;
+using Xunit.Sdk;
+using System.Reflection;
 
 namespace Liviano.Tests.Liviano
 {
+    public class BeforeWalletManagerTest : BeforeAfterTestAttribute
+    {
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            try
+            {
+                Directory.CreateDirectory("data");
+            }
+            catch
+            {
+            }
+            
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            try
+            {
+                Directory.Delete("data", recursive: true);
+            }
+            catch
+            {
+            }
+        }
+    }
+
     public class WalletManagerTest
     {
         ILogger _Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        
 
         [Fact]
+        [BeforeWalletManagerTest]
         public void CreateWalletWithoutPassword()
         {
-            var wm = new WalletManager(_Logger, Network.Main);
+            var wm = new WalletManager(_Logger, Network.Main, "wmtest");
 
-            // Create data directory if it doesn't exists
-            if (!Directory.Exists("data"))
-                Directory.CreateDirectory("data");
-
-            var mnemonic = wm.CreateWallet("Wallet Manager Test Wallet");
+            var mnemonic = wm.CreateWallet("wmtest");
 
             Assert.NotNull(mnemonic);
             Assert.NotNull(wm.GetWallet());
