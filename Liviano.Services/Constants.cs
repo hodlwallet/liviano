803e5d2bcad28ac04a1592210bb6ae6fce314a45
Igor Guerrero <igorgue@protonmail.com>
2022-01-28 14:38:56 -0600

    Fix Constant.cs file, it needs the root of the API

--- Liviano.Services/Constants.cs
+++ Liviano.Services/Constants.cs
@@ -2,6 +2,6 @@ namespace Liviano.Services
 {
     public static class Constants
     {
-        public const string MEMPOOL_SPACE_2H_STATS = "https://mempool.space/api/v1/statistics/2h";
+        public const string MEMPOOL_SPACE_2H_STATS = "https://mempool.space/api/v1";
     }
 }
